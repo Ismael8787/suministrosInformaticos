c02f53604e77936df66bdcd3a8e4d4917354cbf6
DESKTOP-9JU1DPK\Ismael Del Valle <ismaeldelvalle66@gmail.com>
2026-01-10 02:00:35 +0100

    estilo del listado de productos hecho

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash,session
 from models import Proveedor, Producto, Pedido, DetallePedido, Cliente
 import db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'circuit-box-dev-key'
 
 @app.route('/')
 def home():
@@ -12,6 +13,11 @@ def home():
 @app.route('/registro')
 def registro():
     return render_template("registro.html")
+
+@app.route('/productos')
+def productos():
+    return render_template("productos.html")
+
 
 @app.route('/registro-cliente', methods=['POST'])
 def crearCliente():
@@ -34,6 +40,19 @@ def crearCliente():
         return redirect(url_for('registro'))
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    cliente = db.session.query(Cliente).filter_by(email=email).first()
+
+    if cliente and check_password_hash(cliente.password, password) :
+        session['userId'] = cliente.id
+        session['username'] = cliente.username
+        return redirect(url_for('productos'))
+    else:
+        flash('Usuario o contraseña incorrectos')
+        return redirect(url_for('home'))
 
 
 
